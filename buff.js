23dfb218f85751d2078b8150aaa2a8526ee76e86
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-03 21:52:23 +0800

    backup firebase cloud function

--- buff.js
+++ buff.js
@@ -1,13 +1,26 @@
-var tag = 'angular';
-var id = null;
-var cumulative = 0;
-// db.collection('tutorial_tags').where('tag','==',tag).get().then( (snapshot) => {
-//     snapshot.forEach( (doc) => { id = doc.id; cumulative = doc.data().cumulative + 1 } );
-// })
-// if(id!=null)
-// db.collection('tutorial_tags').doc(id).set({'cumulative': cumulative})
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// db.collection('tutorial_tag').where('tag','==',tag).get().then( (snapshot) => {
-//   snapshot.forEach( (doc) => { console.log(doc.id , '=>' , doc.data() )} );
-// })
+const db = admin.firestore();
 
+exports.updateTag = functions.firestore
+    .document('tutorials/{doc-id}')
+    .onCreate( (snap, context) => {
+        const newValue = snap.data();
+        const tag = newValue.tag;
+        db.collection('tutorial_tags').where('tag','==',tag).get().then( (snapshot) => {
+            snapshot.forEach( (doc) => { 
+              let id = doc.id; 
+              let cumulative = doc.data().cumulative + 1 
+        
+              console.log('id is: ',id, 'cumulative is: '+cumulative);
+              if(id!=null)
+              db.collection('tutorial_tags').doc(id).update({'cumulative': cumulative})
+        
+              db.collection('tutorial_tags').where('tag','==',tag).get().then( (snapshot) => {
+                snapshot.forEach( (doc) => { console.log(doc.id , '=>' , doc.data() )} );
+              });
+            } );
+        })
+    });
