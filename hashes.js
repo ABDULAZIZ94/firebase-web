405143227cc5d0a44fe987d3c4c2e2fbf677ae02
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-06 16:31:06 +0800

    found use of set({merge: true})

--- hashes.js
+++ hashes.js
@@ -1,4 +1,6 @@
-const admin = require('firebase-admin');  
+var uid = require ('uid');
+
+const admin = require('firebase-admin');
 let serviceAccount = require('./key/admin-key.json');
 
 admin.initializeApp({
@@ -10,5 +12,32 @@ let db = admin.firestore();
 // db.collection('hashes').doc('0').get().then( (doc)=>{
 //     console.log(doc.id,'=>',doc.data().indexes);
 // })
+hash_id = uid();
+console.log(hash_id);
 
-db.collection('hashes').doc('0').update({'dfer345ertge5':{'tag':'code','title':'How to Code'}} );
+// console.log(data);
+
+//ok
+var data = '{ "'+hash_id+'" :{"tag":"code","title":"How to Code"}}';
+try{
+  db.collection('hashes').doc('0').set(JSON.parse(data),{merge:true});
+}catch(err){
+  
+}
+
+
+//read ok
+// var d;
+// db.collection('hashes').doc('0').get()
+//   .then((doc) => {
+//       console.log(doc.id, '=>', d = doc.data());
+
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
+
+//   var obj = JSON.parse(d);
+//   for(i in obj){
+//     console.log(i);
+//   }
